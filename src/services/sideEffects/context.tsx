5e3c254006e8910fb1c0616f1fa31a519d98395f
mano92fuentesjimenez <m92fuentes@gmail.com>
2020-12-15 14:24:10 -0500

    give sideEffects access to other sideEffects

--- src/services/sideEffects/context.tsx
+++ src/services/sideEffects/context.tsx
@@ -1,7 +1,8 @@
 import {createContext, FC} from "react";
 import { useStore } from "react-redux";
 import sideEffects from "./sideEffects";
-import {SideEffectBody, SideEffects} from "types/sideEffect";
+import { SideEffects } from "types/sideEffect";
+import {AppState} from "../../types/appState";
 
 const context = createContext<SideEffects>({});
 context.displayName = 'SideEffects context';
@@ -10,11 +11,17 @@ export const SideEffectContext = context;
 export const SideEffectConsumer = context.Consumer;
 
 export const SideEffectProvider:FC = ({ children }) => {
-    const store = useStore();
+    const store = useStore<AppState>();
 
-    const values = Object.entries(sideEffects)
-        .map<[string, SideEffectBody]>(([key, s]) => [key, s(store)])
-        .reduce((previous, [key, v]) => ({ [key]: v, ...previous }), {});
+    const values = {};
+    Object.entries(sideEffects)
+        .forEach(([key, s]) => {
+            const f = s(store);
+            // so all sideEffects have access to others sideEffects
+            f.bind(values);
+
+            values[key] = f;
+        })
 
     return <context.Provider value={values}>
         {children}
