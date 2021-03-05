40b983d4c8b14d8f54ede457595387b67fe65240
mano92fuentesjimenez <m92fuentes@gmail.com>
2021-03-04 22:28:28 -0500

    fix loading daysWithData

--- src/scenes/VariableViewer/sagas.ts
+++ src/scenes/VariableViewer/sagas.ts
@@ -1,13 +1,22 @@
-import { call, takeLatest, put } from 'redux-saga/effects'
+import { call, takeLatest, put, select } from 'redux-saga/effects'
 import { ISagasDependencies } from 'types/app'
 import {
   GET_AVAILABLE_DATA_IN_MONTH,
   getAvailableDataInMonthSuccessful,
+  SET_CURRENT_DATE,
 } from './actions'
 import Api from 'services/api'
+import { selectDaysWithData } from './selectors'
+import { getMonth } from 'date-fns'
 
 function* getAvailableDataInMonth(api: Api, action) {
   const date = action.payload
+
+  const currentData = yield select(selectDaysWithData)
+
+  if (currentData.length > 0 && getMonth(currentData) === getMonth(date)) {
+    return yield put(getAvailableDataInMonthSuccessful(currentData))
+  }
 
   const datesWithData = yield call(api.fetchAvailableDataInMonth, date)
   yield put(getAvailableDataInMonthSuccessful(datesWithData))
